7a7d0ee0e87341a3452a076fcc795cb2038efab3
Anna <nana.clafoutis@gmail.com>
2023-11-20 17:48:37 +0100

    ajout du routing

---
+++ src/app/components/start-button/start-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'start-button',
+  templateUrl: './start-button.component.html',
+  styleUrls: ['./start-button.component.scss']
+})
+export class StartButtonComponent {
+
+}
