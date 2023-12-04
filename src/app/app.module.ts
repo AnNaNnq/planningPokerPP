f3af48041107450b8ac8b3a82b89a517cd78db42
Anna <nana.clafoutis@gmail.com>
2023-12-04 16:17:20 +0100

    rajout de composants et d'un modèle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartButtonComponent } from './components/start-button/start-button.component';
+import { InformationButtonComponent } from './components/information-button/information-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartButtonComponent,
+    InformationButtonComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/information-button/information-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-information-button',
+  templateUrl: './information-button.component.html',
+  styleUrls: ['./information-button.component.scss']
+})
+export class InformationButtonComponent {
+
+}
