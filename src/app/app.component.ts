c33252e8b8d5c1483f773763f9a42fc150bf746a
Xoletis <47059364+Xoletis@users.noreply.github.com>
2023-12-11 17:14:20 +0100

    je sais pas ce que j'ai changé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './components/app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
