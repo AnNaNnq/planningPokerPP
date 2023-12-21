b8650ea3c6bad62d1c80adcf4f401964d8891c28
Xoletis <47059364+Xoletis@users.noreply.github.com>
2023-12-21 12:03:39 +0100

    Majorité Absolu

--- src/app/classes/decorateur/MajabsDecorator.ts
+++ src/app/classes/decorateur/MajabsDecorator.ts
@@ -23,17 +23,59 @@ export class MajabsDecorator extends ModeDecorator{
     Object.keys(this.getNotes()).forEach(key => {
       notesCount[parseInt(this.getNote(key))] += 1;
     });
-
+    let value = 0;
     Object.keys(notesCount).forEach(key => {
       if(notesCount[parseInt(key)] > (this.getPlayerNumber()/2)){
         absMaj = true
+        value = parseInt(key);
       }
     });
 
     const balise = HtmlBalise.getInstance();
-
+    let nb = 0;
     if(absMaj){
+      this.html.addText("The absolute majority has to win. The note : ")
 
+      Object.keys(this.getBacklogData()).forEach(key => {
+        nb++;
+        if (nb == this.getActualStage()) {
+          this.modifyBacklogData(key, value)
+          this.html.addText(value + "")
+        }
+      })
+
+      this.html.displayHTML(balise.gameModeMessage);
+      this.retry = false;
+    }else{
+      let max = 0;
+      let maxPlayer = "";
+
+      let min = 1000;
+      let minPlayer = "";
+
+      Object.keys(this.getNotes()).forEach(key => {
+        if(parseInt(this.getNote(key)) > max){
+          max = parseInt(this.getNote(key));
+          maxPlayer = key;
+        }
+        if(parseInt(this.getNote(key)) < min){
+          min = parseInt(this.getNote(key));
+          minPlayer = key;
+        }
+      });
+
+      this.html.addHtmlElement("b");
+      this.html.addText(maxPlayer);
+      this.html.addHtmlElement("b", undefined, undefined, true);
+      this.html.addText(" has the highest rating they will explain his choice then ");
+      this.html.addHtmlElement("b");
+      this.html.addText(minPlayer);
+      this.html.addHtmlElement("b", undefined, undefined, true);
+      this.html.addText(" with the lowest score will be explained");
+
+      this.html.displayHTML(balise.gameModeMessage);
+
+      this.retry = true;
     }
 
     balise.validateButton.style.display = 'block';
